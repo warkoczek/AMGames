795ff793d78da098cebdd4985f270c2935baa3fe
warkoczek <53393030+warkoczek@users.noreply.github.com>
2020-08-02 15:28:33 +0200

    1 commit

---
+++ src/main/java/com/example/AMGames/AmGamesApplication.java
@@ -0,0 +1,13 @@
+package com.example.AMGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AMGames/AmGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AMGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
